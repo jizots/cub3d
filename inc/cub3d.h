d385fee86647849f8ff60e6403c9c5125a93a551
jizots <hotphilly1776@yahoo.co.jp>
2023-09-18 15:39:23 +0900

    add files

---
+++ inc/cub3d.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 18:30:22 by sotanaka          #+#    #+#             */
+/*   Updated: 2023/09/18 15:39:01 by sotanaka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+/*------include-------*/
+/*
+open, close, read, write,printf, malloc, free, perror,strerror, exit
+• All functions of the math library (-lm man man 3 math)
+• All functions of the MinilibX
+*/
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <math.h>
+# include <stdbool.h>
+
+/*-------difine------*/
+/*------typedef------*/
+
+/*------prototype------*/
+
+
+#endif
