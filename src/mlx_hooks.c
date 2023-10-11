d06b70508d8a5821d6e5310fedb6d912d74ee6a2
sota <hotphilly1776@yahoo.co.jp>
2023-10-11 16:07:19 +0900

    use wasd key Fixes #34

--- src/mlx_hooks.c
+++ src/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: hotph <hotph@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 17:45:33 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/10/10 21:10:24 by hotph            ###   ########.fr       */
+/*   Updated: 2023/10/11 16:06:08 by hotph            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	human_walk2(t_human *human, t_mlx *mlx, int keycode, t_point2df old_pos)
 {
-	if (keycode == KEY_RIGHT)
+	if (keycode == KEY_D)
 	{
 		human->point.y += sin(human->vector + get_radian(90)) * MOVE_SPEED;
 		if (is_wall(human->point, mlx))
@@ -24,7 +24,7 @@ void	human_walk2(t_human *human, t_mlx *mlx, int keycode, t_point2df old_pos)
 		if (is_wall(human->point, mlx))
 			human->point.x = old_pos.x;
 	}
-	else if (keycode == KEY_LEFT)
+	else if (keycode == KEY_A)
 	{
 		human->point.y += sin(human->vector + get_radian(-90)) * MOVE_SPEED;
 		if (is_wall(human->point, mlx))
@@ -40,7 +40,7 @@ void	human_walk(t_human *human, t_mlx *mlx, int keycode)
 	t_point2df		old_pos;
 
 	old_pos = human->point;
-	if (keycode == KEY_DOWN)
+	if (keycode == KEY_S)
 	{
 		human->point.y -= sin(human->vector) * MOVE_SPEED;
 		if (is_wall(human->point, mlx))
@@ -49,7 +49,7 @@ void	human_walk(t_human *human, t_mlx *mlx, int keycode)
 		if (is_wall(human->point, mlx))
 			human->point.x = old_pos.x;
 	}
-	else if (keycode == KEY_UP)
+	else if (keycode == KEY_W)
 	{
 		human->point.y += sin(human->vector) * MOVE_SPEED;
 		if (is_wall(human->point, mlx))
@@ -64,9 +64,9 @@ void	human_walk(t_human *human, t_mlx *mlx, int keycode)
 
 void	human_vector_rotate(t_human *human, int keycode)
 {
-	if (keycode == KEY_A)
+	if (keycode == KEY_LEFT)
 		human->vector -= 0.1;
-	else if (keycode == KEY_D)
+	else if (keycode == KEY_RIGHT)
 		human->vector += 0.1;
 	if (human->vector < 0)
 		human->vector += 2 * M_PI;
@@ -84,9 +84,9 @@ int	cub3d_mlx_keypush(int keycode, t_meta *meta)
 		free_meta(meta);
 		return (my_mlx_close_win(&(meta->mlx)));
 	}
-	else if (KEY_LEFT <= keycode && keycode <= KEY_UP)
+	else if ((KEY_A <= keycode && keycode <= KEY_D) || keycode == KEY_W)
 		human_walk(&(meta->human), &(meta->mlx), keycode);
-	else if (keycode == KEY_A || keycode == KEY_D)
+	else if (keycode == KEY_LEFT || keycode == KEY_RIGHT)
 		human_vector_rotate(&(meta->human), keycode);
 	cub3d_draw_view(meta);
 	cub3d_draw_human(meta);
