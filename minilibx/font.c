8547371e0177c32b98aec44a3de76a84d6bf8237
sota <hotphilly1776@yahoo.co.jp>
2023-10-11 09:43:20 +0900

    inversion image Fixes #28

--- minilibx/font.c
+++ minilibx/font.c
@@ -1,10 +1,11 @@
 /* GIMP RGBA C-Source image dump (font.c) */
 
-static const struct {
-  unsigned int 	 width;
-  unsigned int 	 height;
-  unsigned int 	 bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */ 
-  unsigned char	 pixel_data[1140 * 20 * 4 + 1];
+static const struct
+{
+	unsigned int	width;
+	unsigned int	height;
+	unsigned int	bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */ 
+	unsigned char	pixel_data[1140 * 20 * 4 + 1];
 } font_atlas = {
   1140, 20, 4,
   "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
