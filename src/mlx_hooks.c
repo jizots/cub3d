79c5da432f44937ab88fb469df8a0eaffebbead8
jizots <hotphilly1776@yahoo.co.jp>
2023-10-19 19:32:13 +0900

    Moving speed: when big map Fixes #50

--- src/mlx_hooks.c
+++ src/mlx_hooks.c
@@ -6,63 +6,66 @@
 /*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 17:45:33 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/10/19 14:26:52 by sotanaka         ###   ########.fr       */
+/*   Updated: 2023/10/19 19:29:14 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "draw.h"
 #include "keycode.h"
 
-void	human_walk2(t_human *human, t_mlx *mlx, int keycode, t_point2df old_pos)
+static void	human_walk2(t_human *human, t_mlx *mlx, int keycode, double speed)
 {
+	t_point2df		old_pos;
+
+	old_pos = human->point;
 	if (keycode == KEY_D)
 	{
-		human->point.y += sin(human->vector + get_radian(90)) * MOVE_SPEED;
+		human->point.y += sin(human->vector + get_radian(90)) * speed;
 		if (is_wall((t_point2di){human->point.x, human->point.y}, mlx))
 			human->point.y = old_pos.y;
-		human->point.x += cos(human->vector + get_radian(90)) * MOVE_SPEED;
+		human->point.x += cos(human->vector + get_radian(90)) * speed;
 		if (is_wall((t_point2di){human->point.x, human->point.y}, mlx))
 			human->point.x = old_pos.x;
 	}
 	else if (keycode == KEY_A)
 	{
-		human->point.y += sin(human->vector + get_radian(-90)) * MOVE_SPEED;
+		human->point.y += sin(human->vector + get_radian(-90)) * speed;
 		if (is_wall((t_point2di){human->point.x, human->point.y}, mlx))
 			human->point.y = old_pos.y;
-		human->point.x += cos(human->vector + get_radian(-90)) * MOVE_SPEED;
+		human->point.x += cos(human->vector + get_radian(-90)) * speed;
 		if (is_wall((t_point2di){human->point.x, human->point.y}, mlx))
 			human->point.x = old_pos.x;
 	}
 }
 
-void	human_walk(t_human *human, t_mlx *mlx, int keycode)
+static void	human_walk(t_human *human, t_mlx *mlx, int keycode, double speed)
 {
 	t_point2df		old_pos;
 
 	old_pos = human->point;
 	if (keycode == KEY_S)
 	{
-		human->point.y -= sin(human->vector) * MOVE_SPEED;
+		human->point.y -= sin(human->vector) * speed;
 		if (is_wall((t_point2di){human->point.x, human->point.y}, mlx))
 			human->point.y = old_pos.y;
-		human->point.x -= cos(human->vector) * MOVE_SPEED;
+		human->point.x -= cos(human->vector) * speed;
 		if (is_wall((t_point2di){human->point.x, human->point.y}, mlx))
 			human->point.x = old_pos.x;
 	}
 	else if (keycode == KEY_W)
 	{
-		human->point.y += sin(human->vector) * MOVE_SPEED;
+		human->point.y += sin(human->vector) * speed;
 		if (is_wall((t_point2di){human->point.x, human->point.y}, mlx))
 			human->point.y = old_pos.y;
-		human->point.x += cos(human->vector) * MOVE_SPEED;
+		human->point.x += cos(human->vector) * speed;
 		if (is_wall((t_point2di){human->point.x, human->point.y}, mlx))
 			human->point.x = old_pos.x;
 	}
 	else
-		human_walk2(human, mlx, keycode, old_pos);
+		human_walk2(human, mlx, keycode, speed);
 }
 
-void	human_vector_rotate(t_human *human, int keycode)
+static void	human_vector_rotate(t_human *human, int keycode)
 {
 	if (keycode == KEY_LEFT)
 		human->vector -= 0.07;
@@ -83,7 +86,7 @@ int	cub3d_mlx_keypush(int keycode, t_meta *meta)
 		return (my_mlx_close_win(&(meta->mlx)));
 	}
 	else if ((KEY_A <= keycode && keycode <= KEY_D) || keycode == KEY_W)
-		human_walk(&(meta->human), &(meta->mlx), keycode);
+		human_walk(&(meta->human), &(meta->mlx), keycode, meta->tile_size / 4.0);
 	else if (keycode == KEY_LEFT || keycode == KEY_RIGHT)
 		human_vector_rotate(&(meta->human), keycode);
 	return (0);
